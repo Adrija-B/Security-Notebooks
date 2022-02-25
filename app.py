2575af49feeeb8c8893b24899b6d95be94ff3cba
Adrija-B <89545293+Adrija-B@users.noreply.github.com>
2022-02-25 12:01:27 -0700

    Class 7

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,12 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 # venv/Scripts/Activate.ps1
+
+
+@app.route("/predict", methods =["POST"]) # @ sign wraps the code for the route
+def predict():
+    data = request.get_json()
+    clf.predict(data)
+    return predictions #json
+    predictions = [[1]]
+    
